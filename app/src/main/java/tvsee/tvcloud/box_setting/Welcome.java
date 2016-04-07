5623347eefd5687fec221e98316c515905ebddc4
brooze.wang <brooze.wang@yahoo.com>
2016-04-07 10:24:16 +0800

    Initial commit

---
+++ app/src/main/java/tvsee/tvcloud/box_setting/Welcome.java
@@ -0,0 +1,13 @@
+package tvsee.tvcloud.box_setting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
